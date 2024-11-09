306981b94e01a45a017b683b478d0a82b0edba8b
NAYANA P <nayanapvijay@gmail.com>
2024-11-09 13:33:58 +0530

    DOM selectors applied

---
+++ script.js
@@ -0,0 +1,23 @@
+// let element = document.getElementById("main-title")
+// console.log(element)
+
+// let element = document.getElementsByClassName("container")
+// console.log(element)
+
+// let element = document.getElementsByClassName("section")
+// console.log(element)
+
+// let element = document.getElementsByTagName("p")
+// console.log(element)
+
+// let element = document.querySelector(".btn")
+// console.log(element)
+
+// let element = document.querySelector("p")
+// console.log(element)
+
+// let element = document.querySelectorAll(".btn")
+// console.log(element)
+
+let element = document.querySelectorAll("p")
+console.log(element)
